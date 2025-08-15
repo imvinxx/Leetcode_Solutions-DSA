044482b27c1b1d0855c2de66c18e0dd12843ae1b
Vinay Kumar <133787365+imvinxx@users.noreply.github.com>
2025-08-15 18:41:27 +0530

    Time: 0 ms (100%), Space: 7.8 MB (52.41%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if (n==0) return false;
+        while (n!=0){
+            if (n == 1) return true;
+            else if (n % 4 != 0) return false;
+            else n=n/4;
+        } 
+        return true;
+    }
+};
